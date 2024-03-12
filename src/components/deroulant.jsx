22f9fe52b323bed33243926fa44030b663ea22dc
Jean Vuillet <vuillet433@hotmail.com>
2024-03-11 20:10:30 -0500

    first hook ok

--- src/components/deroulant.jsx
+++ src/components/deroulant.jsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 export function Deroulant({title, list}){
+let [display,setDisplay]=useState('flex');
+
+function DisplaySeter(){
+setDisplay((display=='flex')? 'none':'flex');
+}
     let content;
 if (list.map){
   content=list.map((element,index)=>(<div key={index}>{element}</div>))}
     else{ content=list;}
     return(
 <div>
-    <h1>{title}</h1>
-    <ul>
+    <h1 onClick={DisplaySeter}>{title}</h1>
+    <ul className="content" style={{display:display}}>
  {content}
     </ul>
 </div>
