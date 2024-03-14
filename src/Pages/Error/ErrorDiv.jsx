c65cb53e1b52a55ff5a5b57502c22e8e989b931e
Jean Vuillet <vuillet433@hotmail.com>
2024-03-14 10:41:19 -0500

    errorPage better

--- src/Pages/Error/ErrorDiv.jsx
+++ src/Pages/Error/ErrorDiv.jsx
@@ -1,13 +1,15 @@
-import { Link } from "react-router-dom";
-import Home from "../Home/Home";
+import { NavLink } from "react-router-dom";
+import './Error.scss';
 
 export function ErrorDiv(){
 
     return(
         <>
-        <div>404</div>
-        <div>Oups! La page que vous demandez n'existe pas.</div>
-        <Link to='/'>Retourner sur la page d’accueil</Link>
+        <div className="errorDiv">
+        <div className="errorNumber">404</div>
+        <div className="errorMessage">Oups! La page que vous demandez n'existe pas.</div>
+        <NavLink  className='link'  to='/'>Retourner sur la page d’accueil</NavLink>
+        </div>
         </>
     )
 
