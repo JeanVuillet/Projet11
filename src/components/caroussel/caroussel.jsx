d3e8827c1d43b3a01f7201b5d06cfeb3c3e24344
Jean Vuillet <vuillet433@hotmail.com>
2024-03-14 19:14:36 -0500

    caroussel better

--- src/components/caroussel/caroussel.jsx
+++ src/components/caroussel/caroussel.jsx
@@ -1,5 +1,6 @@
 
 import { useState } from "react"
+import './caroussel.scss'
 
 export function Caroussel ({appartement }){
 let [position, setPosition]=useState(1)
@@ -13,28 +14,38 @@ function goForward(){
  }
 
 return(
-    <div>
-       <svg
-       onClick={goBack}
-      className="arrow"
+    <div className="carousselDiv">
+         <svg
+         onClick={goBack}
+      className="leftArrow"
       width="24"
       height="14"
       viewBox="0 0 24 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ transform: `rotate(${45}deg)`, transition: 'transform 0.3s' , width:'5rem',height:'5rem', backgroundColor:'red'}}
-    ></svg>
+      style={{ transform: `rotate(${-90}deg)`,  }}
+    >
+      <path
+        d="M10.7897 0.789662C11.4591 0.120243 12.5462 0.120243 13.2157 0.789662L23.4979 11.0719C24.1674 11.7414 24.1674 12.8285 23.4979 13.4979C22.8285 14.1673 21.7414 14.1673 21.072 13.4979L12 4.42595L2.92804 13.4926C2.25862 14.162 1.17148 14.162 0.502064 13.4926C-0.167355 12.8231 -0.167355 11.736 0.502064 11.0666L10.7843 0.784307L10.7897 0.789662Z"
+        fill="white"
+      />
+    </svg>
     <img src={appartement.pictures[position]}></img>
     <svg
-     onClick={goForward}
-      className="arrow"
+    onClick={goForward}
+      className="rightArrow"
       width="24"
       height="14"
       viewBox="0 0 24 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ transform: `rotate(${-45}deg)`, transition: 'transform 0.3s' ,width:'5rem',height:'5rem', backgroundColor:'blue'}}
-    ></svg>
+      style={{ transform: `rotate(${90}deg)`,  }}
+    >
+      <path
+        d="M10.7897 0.789662C11.4591 0.120243 12.5462 0.120243 13.2157 0.789662L23.4979 11.0719C24.1674 11.7414 24.1674 12.8285 23.4979 13.4979C22.8285 14.1673 21.7414 14.1673 21.072 13.4979L12 4.42595L2.92804 13.4926C2.25862 14.162 1.17148 14.162 0.502064 13.4926C-0.167355 12.8231 -0.167355 11.736 0.502064 11.0666L10.7843 0.784307L10.7897 0.789662Z"
+        fill="white"
+      />
+    </svg>
     </div>
 )
 
