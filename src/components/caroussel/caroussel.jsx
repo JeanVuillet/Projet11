bd3df55fbb6e4b3c55caa6a70b784e901657a39f
Jean Vuillet <vuillet433@hotmail.com>
2024-03-19 16:20:54 -0500

    caroussel almost perfect

--- src/components/caroussel/caroussel.jsx
+++ src/components/caroussel/caroussel.jsx
@@ -3,13 +3,20 @@ import { useState } from "react"
 import './caroussel.scss'
 
 export function Caroussel ({appartement }){
-let [position, setPosition]=useState(1)
+let [position, setPosition]=useState(0)
+
+
+let arrows;
+
+(appartement.pictures.length-1)?arrows='flex':arrows='none';
+
+
 function goBack(){
-   setPosition( position===0? appartement.pictures.length -1 :position -1);
+   setPosition( position===0? appartement.pictures.length-1  :position -1);
    console.log(position)
 }
 function goForward(){
-    setPosition( position===appartement.pictures.length -1? 0:position +1);
+    setPosition( position===appartement.pictures.length-1 ? 0:position +1);
     console.log(position)
  }
 
@@ -23,7 +30,7 @@ return(
       viewBox="0 0 24 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ transform: `rotate(${-90}deg)`,  }}
+      style={{ transform: `rotate(${-90}deg)`,  display:arrows}}
     >
       <path
         d="M10.7897 0.789662C11.4591 0.120243 12.5462 0.120243 13.2157 0.789662L23.4979 11.0719C24.1674 11.7414 24.1674 12.8285 23.4979 13.4979C22.8285 14.1673 21.7414 14.1673 21.072 13.4979L12 4.42595L2.92804 13.4926C2.25862 14.162 1.17148 14.162 0.502064 13.4926C-0.167355 12.8231 -0.167355 11.736 0.502064 11.0666L10.7843 0.784307L10.7897 0.789662Z"
@@ -31,6 +38,7 @@ return(
       />
     </svg>
     <img src={appartement.pictures[position]} className="appartImg"></img>
+    <div   style={{display:arrows}}>{position}</div>
     <svg
     onClick={goForward}
       className="rightArrow"
@@ -39,7 +47,7 @@ return(
       viewBox="0 0 24 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      style={{ transform: `rotate(${90}deg)`,  }}
+      style={{ transform: `rotate(${90}deg)`, display: arrows }}
     >
       <path
         d="M10.7897 0.789662C11.4591 0.120243 12.5462 0.120243 13.2157 0.789662L23.4979 11.0719C24.1674 11.7414 24.1674 12.8285 23.4979 13.4979C22.8285 14.1673 21.7414 14.1673 21.072 13.4979L12 4.42595L2.92804 13.4926C2.25862 14.162 1.17148 14.162 0.502064 13.4926C-0.167355 12.8231 -0.167355 11.736 0.502064 11.0666L10.7843 0.784307L10.7897 0.789662Z"
